335b08cb559cd7cd041dc83d4d9b4f43918c570f
Bruna-Mariana <brunamarianapessoal@gmail.com>
2022-01-24 13:34:09 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// Crie uma variável que receba um nome;
+let nome = "Rhuan"
+
+// Crie uma variável que receba um número;
+let numero = 15
+
+// Crie uma variável com o ano atual e subtraia pelo número que você guardou;
+let ano = 2022
+let novoAno = ano - numero
+
+// Agora mostre no console o resultado da soma das duas variáveis guardadas anteriormente
+console.log(novoAno)
+
+//Mostre no console a mensagem "Estou mexendo no console.log";
+console.log("Estou mexendo no console.log")
+
+// Crie uma variável chamada “quartaFeira” e atribua à ela o valor booleano que representa verdadeiro ou falso e mostre a saída no console;
+let quartaFeira = true
+console.log(quartaFeira)
+
+// Exiba no console o tipo de dado da váriavel quartaFeira;
+console.log(typeof quartaFeira)
